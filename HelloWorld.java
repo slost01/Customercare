8b0b1aefd5a294b752f9aec327a855bb0f6d1736
root <stk@telenet.be>
2021-05-03 20:47:27 +0000

    Added README.md file and added comment lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+//This program will print Hello World
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
---
+++ README.md
@@ -0,0 +1,7 @@
+Application name: Customer Care
+
+Platform: Linux
+
+Language: Java
+
+team Members: Dev A, Dev B
