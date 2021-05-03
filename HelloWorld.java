771db5a99f5b71c3e043b84f28cd4347d722bd98
root <stk@telenet.be>
2021-05-03 21:27:45 +0000

    Restored the file from second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
-//This program will print Hello World
 public class HelloWorld {
    public static void main(String[] args) {
-
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-      // Prints "Welcome to Git" in the terminal window.
-      System.out.println("Welcome to Git !!"); 
    }
 }
