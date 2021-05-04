b684a16e02e7564aeea6264ebd2da5459168a7a8
root <stk@telenet.be>
2021-05-04 12:16:50 +0000

    Added part of facebook-int

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
    public static void main(String[] args) {
+
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
+ 
+      // Prints "Welcome to Facebook" in the terminal window.
+      System.out.println("Welcome to Facebook");
    }
 }
